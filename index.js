bed6a9301f239adcd9f1c87f6b0fa710e2a88e37
Dominykas Blyžė <hello@dominykas.com>
2015-05-24 12:50:10 +0300

    split up into smaller modules

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 "use strict";
 
-function check() {
-	// GET pkg info
-	throw new Error("Not implemented");
-}
-
-function download(options) {
-
-	// GET pkg > /tmp/pkg.zip
-	// extract(/tmp/pkg.zip, options.extractTo)
-	// verify(/tmp/pkg.zip, options.extractTo)
-	// if (!noconfirm) confirm()
-	// if (!nocleanup) remove(/tmp/pkg.zip)
-
-	console.log(options);
-	throw new Error("Not implemented");
-}
-
-function verify(pkg, folder) {
-	console.log(pkg, folder);
-	throw new Error("Not implemented");
-}
-
-exports.check = check;
-exports.download = download;
+exports.check = require("./lib/check");
+exports.download = require("./lib/download");
 exports.extract = require("./lib/extract");
-exports.verify = verify;
+exports.verify = require("./lib/verify");
